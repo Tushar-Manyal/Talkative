1c091989c8affea42f53071e6c76063d0b46bde5
Tushar-Manyal <manyaltushar@gmail.com>
2025-05-08 04:43:33 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -55,7 +55,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
     cors({
-        origin:["http://localhost:5173" , "http://localhost:5174"],
+        origin:["http://localhost:5173","https://talkative-frontend-portion.onrender.com"],
         credentials:true,
        
     })
