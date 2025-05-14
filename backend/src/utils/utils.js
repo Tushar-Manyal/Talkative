f1f23e39642dba84f424931210440a6fa46612d7
Tushar Manyal <manyaltushar@gmail.com>
2025-05-15 01:27:07 +0530

    Update utils.js

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -4,10 +4,17 @@ export const generateToken=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{expiresIn:"7d"});
 
     res.cookie("jwt", token, {
-        maxAge:7*24*60*60*1000, //7days
-        httpOnly: true,       // JS can't access it
-        sameSite: 'strict',    // CSRF protection
-        secure: process.env.NODE_ENV !== "development"      // only sent over HTTPS
+        // maxAge:7*24*60*60*1000, //7days
+        // httpOnly: true,       // JS can't access it
+        // sameSite: 'strict',    // CSRF protection
+        // secure: process.env.NODE_ENV !== "development"      // only sent over HTTPS
+
+
+
+        httpOnly: true,  // Cannot access via JS
+        secure: process.env.NODE_ENV === "production",  // Only send over HTTPS in prod
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Allow cross-site cookies in prod
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     return token;
 }
