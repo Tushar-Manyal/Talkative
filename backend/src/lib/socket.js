11bdcdbbc99c68321ab5f0eef6908fc88d831d96
Tushar Manyal <manyaltushar@gmail.com>
2025-06-04 16:26:48 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173","https://talkative-frontend-portion.onrender.com"],
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    credentials:true
   },
 });
 
